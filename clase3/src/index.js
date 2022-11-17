504884a08edd4e250b113691ad2996fe5ba0ec74
andy <arteysoft@gmail.com>
2022-11-16 21:17:15 -0300

    generar archivos

--- clase3/src/index.js
+++ clase3/src/index.js
@@ -2,21 +2,18 @@ let genUsuario = require('./lib/genusuario')
 let asyncForLoop = require('./lib/asyncForLoop')
 let fs = require('fs')
 
-// Funciones asincronicas. Escribir archivo
-/*
-let usuario = genUsuario()
-let txtUsuario = JSON.stringify(usuario)
-let nombreArchivo = ['/var/anulomufa/tickets/', usuario.id, '.json'].join('')
-console.log(nombreArchivo)
-fs.writeFile(nombreArchivo, txtUsuario, 'utf-8', (err) => {
-    console.log(err)
-})
-*/
-
-asyncForLoop(1000, (idx, fnNext) => {
-    console.log(idx)
-    fnNext()
+asyncForLoop(2000, (idx, fnNext) => {
+    let usuario = genUsuario()
+    let txtUsuario = JSON.stringify(usuario)
+    let nombreArchivo = ['/var/anulomufa/tickets/', usuario.id, '.json'].join('')
+    console.log(nombreArchivo)
+    fs.writeFile(nombreArchivo, txtUsuario, 'utf-8', (err) => {
+        console.log(err)
+        fnNext()
+    })
 }, (err) => {
     console.log(err)
 })
+
+
 
