cbb95c8879691e9ffc5e90c28f44c418950a15e1
andy <arteysoft@gmail.com>
2022-11-23 21:07:12 -0300

    clase 5 init

---
+++ clase5/src/index.ts
@@ -0,0 +1,33 @@
+/*
+console.log("hola mundo")
+
+let variable1:number = 20
+let variable2 = variable1
+
+type PredicateFunction = () => boolean
+
+let fn:PredicateFunction
+
+fn = () => {
+    console.log('estoy en fn')
+    return true
+}
+
+let a = fn()
+*/
+
+let esPar:(x:number) => boolean|string = (x) => {
+    if (0) {
+        return "love"
+    }
+    return (x%2===0) ? true : false
+}
+
+let concatenar:(x:string, y:string) => string = (x, y) => {
+    return x.concat(y)
+}
+
+let s = esPar(20)
+
+concatenar(s, s)
+
