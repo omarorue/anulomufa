6c1cea12f754b0e10fd1bf62294e481884b43966
andy <arteysoft@gmail.com>
2022-12-07 21:00:17 -0300

    prueba postman

--- clase9/src/index.ts
+++ clase9/src/index.ts
@@ -2,5 +2,19 @@ import express, {Express} from 'express'
 
 let app:Express = express()
 
-app.listen(3000)
+app.get('/', (request, response) => {
+    response.setHeader('Content-Type', 'text/plain')
 
+    let htmlRespuesta = '<h1>Header uno</h1>'
+
+    response.status(200).send(htmlRespuesta)
+})
+
+app.get('/hola', (request, response) => {
+    response.setHeader('Content-Type', 'text/html')
+    let htmlRespuesta = '<h1>Aca ingresa al metodo /hola</h1>'
+
+    response.status(200).send(htmlRespuesta)
+})
+
+app.listen(3000)
