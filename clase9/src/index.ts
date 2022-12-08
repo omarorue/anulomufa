bc4629143a966f75fdd9d63c5f63ccc5f4126d9b
andy <arteysoft@gmail.com>
2022-12-07 21:33:12 -0300

    probar el post con body y luego insertarlo en mongo

--- clase9/src/index.ts
+++ clase9/src/index.ts
@@ -1,6 +1,8 @@
 import express, {Express} from 'express'
+import generarUsuario from './lib/genusuario'
 
 let app:Express = express()
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.setHeader('Content-Type', 'text/plain')
@@ -10,12 +12,35 @@ app.get('/', (request, response) => {
     response.status(200).send(htmlRespuesta)
 })
 
+app.get('/inventarusuario', (request, response) => {
+    response.setHeader('Content-Type', 'application/json')
+    response.status(200).send(generarUsuario())
+})
+
+// Cual es la idea.
+// Vamos a crear un post /usuario y vamos a insertar un documento en mongo
+// Anteriormente, levantamos el documento de un archivo, y luego lo insertabamos
+// Ahora va a venir por post, en el body
+
+app.post('/usuario', (request, response) => {
+    console.log(request.body)
+    response.status(201).end()
+})
+
 app.get('/hola', (request, response) => {
     response.setHeader('Content-Type', 'text/html')
     let htmlRespuesta = '<h1>Aca ingresa al metodo /hola</h1>'
 
     response.status(200).send(htmlRespuesta)
 })
+
+app.delete('/hola', (request, response) => {
+    response.setHeader('Content-Type', 'text/html')
+    let htmlRespuesta = 'Entro al delete'
+
+    response.status(200).send(htmlRespuesta)
+})
+
 
 /**
  * Ejercicio. Implementar delete y responder un texto solo funcione con delete
